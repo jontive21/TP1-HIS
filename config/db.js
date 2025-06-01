8de78f3161d85ec3e646bf7dbd3cc12bde3918d5
jontive21 <joseeontiveros@sanluis.edu.ar>
2025-06-01 19:56:56 -0300

    coneccion a la db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
-const mysql = require('mysql2/promise');
+require('dotenv').config(); 
+
+const mysql = require('mysql2/promise'); 
+
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',        
-    password: '',        
-    database: 'his_db',
-    waitForConnections: true,
-    connectionLimit: 10
+    host: process.env.DB_HOST,         
+    user: process.env.DB_USER,         
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME,     
+    waitForConnections: true,          
+    connectionLimit: 10                
 });
-module.exports = pool;
-    
+
+module.exports = pool; 
