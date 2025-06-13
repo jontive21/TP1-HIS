cb0bb3c827846647c9a2fbaa4c54adf8ef5d20ff
jontive21 <joseeontiveros@sanluis.edu.ar>
2025-06-13 20:37:12 -0300

    datos ejempl

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -6,15 +6,3 @@ exports.showDashboard = (req, res) => {
     message: null
   });
 };
-exports.showDashboard = (req, res) => {
-    res.render('dashboard/index', {
-        title: 'Dashboard - HIS Internación',
-        user: req.session.user,
-        camasDisponibles: 12,
-        camasTotal: 20,
-        pacientesInternados: 8,
-        admisionesHoy: 3,
-        altasHoy: 2,
-        lastAccess: new Date().toLocaleDateString('es-AR')
-    });
-};
