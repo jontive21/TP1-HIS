ca0cfde72b0b9d83d412de71c6f3ae5a7bbe3481
jontive21 <joseeontiveros@sanluis.edu.ar>
2025-06-14 22:21:40 -0300

    correc admis

--- routes/admisionRoutes.js
+++ routes/admisionRoutes.js
@@ -1,9 +1,14 @@
-// routes/admisionRoutes.js
 const express = require('express');
 const router = express.Router();
 const admisionController = require('../controllers/admisionController');
 
-router.get('/:id', admisionController.showNuevaAdmision);
+// Ruta principal para listar admisiones
+router.get('/', admisionController.listarAdmisiones);
+
+// Ruta para mostrar formulario de nueva admisión
+router.get('/nueva', admisionController.showNuevaAdmision);
+
+// Ruta para procesar asignación de cama
 router.post('/asignar', admisionController.asignarCama);
 
 module.exports = router;
