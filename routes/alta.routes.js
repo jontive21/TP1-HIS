39ea5a26a9af7c36395727355e9c784d80e124ee
jontive21 <joseeontiveros@sanluis.edu.ar>
2025-06-16 11:57:17 -0300

    correc altas

--- routes/alta.routes.js
+++ routes/alta.routes.js
@@ -1,15 +1,18 @@
+// routes/alta.routes.js
+
 const express = require('express');
 const router = express.Router();
 const Alta = require('../models/Alta');
 
-// Listar altas hospitalarias
+// Listar todas las altas
 router.get('/altas', async (req, res) => {
   try {
-    const altas = await Alta.getAll();
+    const altas = await Alta.getAll(); // Debe devolver todas las altas
     res.render('altas/index', { altas });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error al cargar altas');
+    res.status(500).send('Error al cargar altas hospitalarias');
   }
 });
+
 module.exports = router;
